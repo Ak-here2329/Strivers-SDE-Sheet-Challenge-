f4b14fa924426e4257b7099c982e2baf42bc966c
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2023-06-06 11:38:30 +0530

    Create pascalsTriangle.cpp
    Day1

---
+++ pascalsTriangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        ans.push_back({1});
+        if(numRows == 1)
+            return ans;
+        ans.push_back({1,1});
+        if(numRows == 2)
+            return ans;
+        
+        for(int i=2; i<numRows; i++){
+            vector<int> curr;
+            curr.push_back(1);
+            for(int j=0; j<ans[i-1].size() - 1; j++){
+                curr.push_back(ans[i-1][j] + ans[i-1][j+1]);
+            }
+            curr.push_back(1);
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
