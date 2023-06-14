f65836c5649e181328e257c9217ab2ea0d15fef9
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2023-06-14 14:17:13 +0530

    Create palindromePartitioning.cpp

---
+++ Day 9/palindromePartitioning.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+bool checkPalindrome(string &str, int s, int e){
+    while(s <= e){
+        if(str[s++] != str[e--])
+            return false;
+    }
+    return true;
+}
+
+void solve(string &s, int idx, vector<string> &path, vector<vector<string>> &ans){
+    if(idx >= s.size()){
+        ans.push_back(path);
+        return ;
+    }
+
+    for(int i = idx; i<s.size(); i++){
+        if(checkPalindrome(s, idx, i)){
+            path.push_back(s.substr(idx, i - idx + 1));
+            solve(s, i + 1, path, ans);
+            path.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string &s) 
+{
+    // Write your code here.
+    vector<vector<string>> ans;
+    vector<string> path;
+    solve(s, 0, path, ans);
+    return ans;
+}
